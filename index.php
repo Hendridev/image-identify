5acf18b7b4432bf9d9930ad16064309997cdd3ea
Hendridev <hendridevlp@gmail.com>
2021-01-05 21:31:54 +0700

    setting environment

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>image gallery</h1>
+        <div class="raw">
+            <p><span>1.</span> Backgroud Vector modern</p>
+            <div class="image">
+                <img src="upload/raww.jpg" alt="">
+            </div>
+            <p>Ukuran: 2032kb</p> 
+            <p>Type: image/jpg</p>
+        </div>
+    </div>
+    <script src="js/script.js"></script>
+</body>
+</html>
